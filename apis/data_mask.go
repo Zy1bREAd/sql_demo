a92794adf7cda07c07efc92b1c4db1cc55794f87
Ocean Wang <qq1186405248@126.com>
2025-05-23 17:41:04 +0800

    [Update] 优化数据脱敏的正则匹配;适配多个partial模式的数据脱敏;

--- apis/data_mask.go
+++ apis/data_mask.go
@@ -60,24 +60,24 @@ type Desensitizer interface {
 }
 
 type FullDesensitizer struct {
-	Rule RuleConfig
+	Rules RuleConfig
 }
 
 // 全加密模式（顺序：精准匹配 -> 正则匹配）
 func (f *FullDesensitizer) Mask(col string, fieldVal []byte) (string, error) {
-	if slices.Contains(f.Rule.IllegalFields, col) {
-		return f.Rule.MaskValue, nil
+	rule := f.Rules
+	if slices.Contains(rule.IllegalFields, col) {
+		return rule.MaskValue, nil
 	}
-	if f.Rule.Regex {
-		for _, illegalRegex := range f.Rule.IllegalFields {
+	if rule.Regex {
+		for _, illegalRegex := range rule.IllegalFields {
 			re, err := regexp.Compile(illegalRegex)
-			fmt.Println("debug>>", re, illegalRegex)
 			if err != nil {
 				log.Println("Regex Pattern is invalid")
 				return "", err
 			}
 			if re.MatchString(col) {
-				return f.Rule.MaskValue, nil
+				return rule.MaskValue, nil
 			}
 			continue
 		}
@@ -86,23 +86,44 @@ func (f *FullDesensitizer) Mask(col string, fieldVal []byte) (string, error) {
 }
 
 type PartialDesensitizer struct {
-	Rule RuleConfig
+	Rules []RuleConfig
 }
 
 // 部分加密模式
 func (p *PartialDesensitizer) Mask(col string, fieldVal []byte) (string, error) {
-	if !slices.Contains(p.Rule.IllegalFields, col) {
-		return string(fieldVal), nil
-	}
-	if len(fieldVal) > p.Rule.MatchRange.End {
-		for i := p.Rule.MatchRange.Start; i <= p.Rule.MatchRange.End; i++ {
-			fieldVal[i] = 42
+	for _, rule := range p.Rules {
+		if slices.Contains(rule.IllegalFields, col) {
+			if len(fieldVal) > rule.MatchRange.End {
+				for i := rule.MatchRange.Start; i <= rule.MatchRange.End; i++ {
+					fieldVal[i] = 42
+				}
+				continue
+			}
+			return "****", nil
 		}
-		return string(fieldVal), nil
+		if rule.Regex {
+			for _, illegalRegex := range rule.IllegalFields {
+				re, err := regexp.Compile(illegalRegex)
+				if err != nil {
+					log.Println("Regex Pattern is invalid")
+					return "", err
+				}
+				if re.MatchString(col) {
+					if len(fieldVal) > rule.MatchRange.End {
+						for i := rule.MatchRange.Start; i <= rule.MatchRange.End; i++ {
+							fieldVal[i] = 42
+						}
+						continue
+					}
+					return "****", nil
+				}
+				continue
+			}
+		}
+		continue
 	}
-	return "****", nil
-	// log.Println("数据脱敏失败，超过字节长度")
-	// return string(fieldVal), GenerateError("MaskFailed", "range is not match bytes length")
+	// 跳出循环统一返回的是原值
+	return string(fieldVal), nil
 }
 
 // 数据遮罩处理
@@ -111,6 +132,7 @@ func DataMaskHandle(col string, fieldVal *sql.RawBytes) string {
 	// 根据你选择的mask模式返回接口实例，使用接口方法区执行dataMask操作。
 	er := getDesensitizer(mode)
 	if er == nil {
+		// none 模式
 		return string(*fieldVal)
 	}
 	maskVal, err := er.Mask(col, *fieldVal)
@@ -120,23 +142,28 @@ func DataMaskHandle(col string, fieldVal *sql.RawBytes) string {
 	return maskVal
 }
 
-func matchRuleConfig(mode string) RuleConfig {
+func matchRuleConfig(mode string) []RuleConfig {
+	ruleList := []RuleConfig{}
 	for _, ruleConfig := range dataMaskConfig.RuleConfig {
 		if mode == ruleConfig.Mode {
-			return ruleConfig
+			ruleList = append(ruleList, ruleConfig)
 		}
 	}
-	return RuleConfig{}
+	return ruleList
 }
 
 func getDesensitizer(mode string) Desensitizer {
 	lowerStr := strings.ToLower(mode)
-	rule := matchRuleConfig(lowerStr)
+	rules := matchRuleConfig(lowerStr)
 	switch {
 	case lowerStr == "full":
-		return &FullDesensitizer{Rule: rule}
+		// 目前仅返回第一个匹配的
+		if len(rules) > 0 {
+			return &FullDesensitizer{Rules: rules[0]}
+		}
+		return &FullDesensitizer{Rules: RuleConfig{}}
 	case lowerStr == "partial":
-		return &PartialDesensitizer{Rule: rule}
+		return &PartialDesensitizer{Rules: rules}
 	default:
 		log.Println("数据遮罩模式为 none")
 		return nil
